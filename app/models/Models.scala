0a25cac918c99ac5eb072e4c3b976cd46d89102f
井川 司 <tsukasa@imac27.local>
2016-06-08 02:02:26 +0900

    モデル変更（試行錯誤。。。）

--- app/models/Models.scala
+++ app/models/Models.scala
@@ -1,7 +1,7 @@
 package models
 
-import org.joda.time.DateTime
+//import org.joda.time.DateTime
 
-case class Domain(id: Int, domain: String, description: String, created_at: DateTime, updated_at: DateTime, active: Boolean)
+//case class Domain(id: Int, domain: String, description: String, created_at: DateTime, updated_at: DateTime, active: Boolean)
 
-case class Alias(id: Int, domain_id: Int, address: String, goto: String, created_at: DateTime, updated_at: DateTime, active: Boolean)
+//case class Alias(id: Int, domain_id: Int, address: String, goto: String, created_at: DateTime, updated_at: DateTime, active: Boolean)
